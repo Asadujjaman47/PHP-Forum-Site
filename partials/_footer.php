1af2706392ff07536b34081b36ce5bd1b9d1a6fb
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2022-12-16 17:56:03 +0600

    Creating An Online Forum In php From Scratch

---
+++ partials/_footer.php
@@ -0,0 +1,11 @@
+<?php
+echo '
+<div class="container-fluid bg-dark text-light">
+    <p class="text-center mb-0">Copyright iDiscuss Coding Forums 2022 | All rights reserved</p>
+</div>
+
+'
+
+
+?>
+
