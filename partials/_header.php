cbb201a977f816f989a7937b75a71038f1af8bd5
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2022-12-17 22:35:53 +0600

    Creating a Template for Displaying Search Results

--- partials/_header.php
+++ partials/_header.php
@@ -43,9 +43,10 @@ echo '
 
         if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']==true){
             echo '
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <form class="form-inline my-2 my-lg-0" method="get" action="search.php">
+                    <input class="form-control mr-sm-2" name="search" type="search" placeholder="Search" aria-label="Search">
                     <button class="btn btn-success my-2 my-sm-0" type="submit">Search</button>
+                    
                     <p class="text-light my-0 mx-2">Welcome '. $_SESSION['useremail']. ' </p>
                     <a href="partials/_logout.php" class="btn btn-outline-success mx-2">Logout</a>
                 </form>
